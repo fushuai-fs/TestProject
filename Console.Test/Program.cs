177837197d3686c26111a450881c66f442cf780f
fushuai <fushuai_f@126.com>
2017-11-09 16:59:03 +0800

    add

--- Console.Test/Program.cs
+++ Console.Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,37 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            MongodbTest.test();
-             
+            // MongodbTest.test();
+
+            List<testModel> list = new List<testModel>();
+            testModel m = new testModel("jack", 18);
+            list.Add(m);
+            list.Add(m);
+            m = new testModel("jack", 20);
+            list.Add(m);
+            m = new testModel("jack", 21);
+            list.Add(m);
+
+            Console.WriteLine("list.count=" + list.Count);
+
+            list = list.Distinct(new DistinctComparer<testModel>("age")).ToList(); 
+            Console.WriteLine("list.count=" + list.Count);
+
+            //list = list.Distinct(new DistinctComparer<testModel>("name")).ToList();
+            //Console.WriteLine("list.count=" + list.Count);
+
+            List<testModel> list2 = new List<testModel>();
+            m = new testModel("jack", 18);
+            list2.Add(m);
+            m = new testModel("jack11", 20);
+            list2.Add(m);
+            EnumerableCompare<testModel> comparer = new EnumerableCompare<testModel>((x, y) => x.name == y.name);
+            list = list2.Except(list,comparer).ToList(); 
+            Console.WriteLine("list.count=" + list.Count);
+            Console.WriteLine("list2.count=" + list2.Count);
+
             Console.ReadKey();
         }
     }
-  
+
 }
