968ebd12e3fb13a5007bb01bc44a83564f801cc0
fushuai <fushuai_f@126.com>
2017-11-20 16:44:43 +0800

    autoMapper

---
+++ Console.Test/HotelProduct.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTest
+{ 
+    /// <summary>
+    ///  
+    /// </summary>
+   public class HotelProduct
+   { 
+       public long _HotelID1 { get; set; } 
+       public string _HotelCode1 { get; set; } 
+       public string _HotelNameCN1 { get; set; } 
+       public string _HotelNameGB1 { get; set; }  
+       public int _SellStatusType1 { get; set; }    
+       public int _LatestBookDay { get; set; }   
+       public bool _IsValid { get; set; }  
+       public List<Rooms> _Rooms { get; set; } 
+    }
+
+  public class Rooms
+   { 
+       public int _RoomID { get; set; } 
+       public string _RoomCode { get; set; } 
+       public string _RoomNameCN { get; set; } 
+       public string _RoomNameGB { get; set; } 
+       public bool _IsValid { get; set; } 
+       public List<SellRooms> _SellRooms { get; set; }  
+   }
+  public class SellRooms
+   { 
+       public int _SellRoomID { get; set; } 
+       public string _SellRoomCode { get; set; } 
+       public string _SellRoomNameCN { get; set; } 
+      public string _SellRoomNameGB { get; set; } 
+       public string _BedType { get; set; } 
+       public int _LodgersLimit { get; set; } 
+       public string _MealType { get; set; }   
+       public int _CancelType { get; set; }  
+       public string _Remark { get; set; }  
+       public bool _IsValid { get; set; } 
+       public List<Sells> _Sells { get; set; }  
+   }
+  public class Sells
+   { 
+       public string _sellDate1 { get; set; }  
+       public Decimal _price1 { get; set; }  
+       public bool _isValid1 { get; set; } 
+       public string _modifyTime1 { get; set; } 
+   }
+}
---
+++ Console.Test/HotelProductEntity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTest
+{ 
+    /// <summary>
+    ///  
+    /// </summary>
+   public class HotelProductEntity
+   { 
+       public long HotelID { get; set; } 
+       public string HotelCode { get; set; } 
+       public string HotelNameCN { get; set; } 
+       public string HotelNameGB { get; set; }  
+       public int SellStatusType { get; set; }    
+       public int  LatestBookDay { get; set; }   
+       public bool IsValid { get; set; }  
+       public List<RoomsEntity> Rooms { get; set; } 
+    }
+
+  public class RoomsEntity
+   { 
+       public int RoomID { get; set; } 
+       public string RoomCode { get; set; } 
+       public string RoomNameCN { get; set; } 
+       public string RoomNameGB { get; set; } 
+       public bool IsValid { get; set; } 
+       public List<SellRoomsEntity> SellRooms { get; set; }  
+   }
+  public class SellRoomsEntity
+   { 
+       public int SellRoomID { get; set; } 
+       public string SellRoomCode { get; set; } 
+       public string SellRoomNameCN { get; set; } 
+      public string SellRoomNameGB { get; set; } 
+       public string BedType { get; set; } 
+       public int LodgersLimit { get; set; } 
+       public string MealType { get; set; }   
+       public int CancelType { get; set; }  
+       public string Remark { get; set; }  
+       public bool IsValid { get; set; } 
+       public List<SellsEntity> Sells { get; set; }  
+   }
+  public class SellsEntity
+   { 
+       public string SellDate { get; set; }  
+       public Decimal Price { get; set; }  
+       public bool IsValid { get; set; } 
+       public string ModifyTime { get; set; } 
+   }
+}
